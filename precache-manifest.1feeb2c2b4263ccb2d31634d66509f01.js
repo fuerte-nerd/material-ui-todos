f37ec4491015f8dc96f868d11c0547c76ec0136b
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-19 10:36:17 +0100

    Updates

---
+++ precache-manifest.1feeb2c2b4263ccb2d31634d66509f01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27dafc629cc20980d2a53a36328dfd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94d0fc3982fbe89c0a65",
+    "url": "/static/css/main.3a3987f9.chunk.css"
+  },
+  {
+    "revision": "46282525d1245b709f79",
+    "url": "/static/js/2.ffd59c4b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ffd59c4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94d0fc3982fbe89c0a65",
+    "url": "/static/js/main.947c45cf.chunk.js"
+  },
+  {
+    "revision": "b30b8e7de59bd5cac1ed",
+    "url": "/static/js/runtime-main.e7a2d70c.js"
+  }
+]);
